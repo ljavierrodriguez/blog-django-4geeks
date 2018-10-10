b11aecd63e9928c25aeb72540bc876af35f6c8f9
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2018-10-10 09:53:50 -0400

    Fix Categoria Model

--- articles/models.py
+++ articles/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Categoria(models.Model):
     descripcion = models.CharField(max_length=100)
 
-    def __srr__(self):
+    def __str__(self):
         return self.descripcion
 
 
