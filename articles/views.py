7ae036031047ab8469be276be7e3393ca4418175
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2018-10-13 18:49:34 -0400

    Fix View Articles

--- articles/views.py
+++ articles/views.py
@@ -5,6 +5,6 @@ from .models import Articulo
 
 # Create your views here.
 def listado_articulos(request):
-    articulos = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    articulos = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('-fecha_publicacion')
     return render(request, 'articles/index.html', {'articulos':articulos})
 
