947bfeb9acef1847c22385198e138adbfb42ef5b
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2018-10-13 18:46:18 -0400

    Fix View Articles

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Articulo
+
 
 # Create your views here.
+def listado_articulos(request):
+    articulos = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'articles/index.html', {'articulos':articulos})
+
